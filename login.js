47bc133b0f24404d63ab7fea1c39a6aeb723324b
Ajay Birare <ajaybirare516@gmail.com>
2024-01-06 18:12:05 +0530

    remove all useless comments

--- login.js
+++ login.js
@@ -8,14 +8,6 @@ let createAccountForm = document.querySelector(".createAccount");
 let user = document.querySelector("#userName");
 let pass = document.querySelector("#passWord");
 
-//______________________________________________________________________
-// new user data(he is check 2 file )
-// let newUser = document.querySelector("#newUsername");
-// let newPass = document.querySelector("#newPassword");
-
-//______________________________________________________________________
-// const dataBase = [{ userName: "Ajay Birare", passWord: "123" }];
-
 // If localStorage doesn't have userLoginData, initialize it with a default user
 if (localStorage.getItem("userLoginData") === null) {
   const dataBase = [{ userName: "Ajay Birare", passWord: "123" }];
@@ -54,41 +46,7 @@ loginInfoForm.addEventListener("submit", (event) => {
 });
 
 //______________________________________________________________________
-//New Account Information
 
-// createAccountForm.addEventListener("submit", (event) => {
-//   event.preventDefault();
-//   console.log(newUser.value);
-//   console.log(newPass.value);
-
-//   let customerData = JSON.parse(localStorage.getItem("userLoginData"));
-
-//   //   before going to create new account
-//   //   check the user is already here or not
-
-//   let flag = false;
-
-//   customerData.forEach((data) => {
-//     if (data.userName == newUser.value) {
-//       newUser.value = "";
-//       newPass.value = "";
-//       alert("the username is already in use");
-//       flag = true;
-//       //   return;
-//     }
-//   });
-
-//   if (flag == true) {
-//     return;
-//   } else {
-//     storeData(newUser.value, newPass.value);
-//   }
-
-//   newUser.value = "";
-//   newPass = "";
-// });
-
-//______________________________________________________________________
 //Storage
 
 function storeData(username, password) {
